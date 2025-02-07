d945a3fb1485c7b0cdc89448914aa94c13a4fc1e
Ralph King Jr <15459559+kingralph33@users.noreply.github.com>
2025-02-07 00:25:37 -0500

    configure redirects and update styling

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,16 +2,16 @@ const About = () => {
   return (
     <div className="max-w-4xl mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-6">About Me</h1>
-      <p className="text-lg text-[oklch(0.32_0.03_270.43)] text-left">
+      <p className="text-lg text-[oklch(0.32_0.03_270.43)] text-left mb-6">
         Hi there!
       </p>
-      <p className="text-lg text-[oklch(0.32_0.03_270.43)] text-left">
+      <p className="text-lg text-[oklch(0.32_0.03_270.43)] indent-8 text-left mb-2">
         I'm a software developer with a passion for building and problem-solving. 
         My journey into tech began in 2016, transitioning from a background in retail and small business banking.
         Since then, I've gained experience in software quality assurance, full-stack development (Python/Flask/Django and MEAN stack), and DevOps, working with technologies like Java, JavaScript, PHP, and cloud platforms like OpenShift and Azure.  
         Currently, I'm contributing to projects at the Federal Government.  
       </p>
-      <p className="text-lg text-[oklch(0.32_0.03_270.43)] text-left mb-12">
+      <p className="text-lg text-[oklch(0.32_0.03_270.43)] indent-8 text-left mb-12">
         Outside of work, I enjoy spending time with my wonderful wife and daughter. 
         We love exploring new restaurants and new construction residential communities. 
         I'm also currently on a fitness journey, balancing my lifelong love of strength training with a newfound appreciation for cardio.
@@ -26,8 +26,8 @@ const About = () => {
         <div className="text-center">
           <h2 className="text-xl font-bold mb-4 text-[oklch(0.32_0.03_270.43)]">Favorite Sports & Teams</h2>
           <ul className="space-y-2 text-gray-700">
-            <li>American Football - Philadelphia Eagles</li>
-            <li>Ultimate Fighting Championship (UFC)</li>
+            <li>Philadelphia Eagles</li>
+            <li>Ultimate Fighting Championship</li>
           </ul>
         </div>
 
